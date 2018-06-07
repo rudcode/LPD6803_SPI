93bebb72ea3b8c412be3fc69c369e5f02ba634cc
rudcode <rudynurhadi@gmail.com>
2018-06-08 05:56:52 +0700

    beautify

--- LPD6803_SPI.cpp
+++ LPD6803_SPI.cpp
@@ -11,122 +11,118 @@
 #include "SPI.h"
 #include "LPD6803_SPI.h"
 
-//some local variables, ised in isr
-static uint16_t indx=0;
-static uint8_t isDirty;
+// The SPI data register (SPDR) holds the byte which is about to be shifted out the MOSI line
+#define SPI_LOAD_BYTE(data) SPDR=data
+// Wait until last bytes is transmitted
+#define SPI_WAIT_TILL_TRANSMITED while(!(SPSR & (1<<SPIF)))
+
+static uint16_t index = 0;
 static uint16_t prettyUglyCopyOfNumPixels;
 static uint16_t *pixelDataCurrent;	//working pointer
 static uint16_t *pixelData; //pointer to pixel buffer, we cannot access pixels form isr!
-volatile unsigned char nState=1;
+volatile unsigned char nState = 1;
 
 // Constructor for use with hardware SPI (specific clock/data pins):
 LPD6803_SPI::LPD6803_SPI(uint16_t n) {
-  prettyUglyCopyOfNumPixels = n;  
-  numLEDs = n;  
-  pixelData = (uint16_t *)malloc(n);
-  isDirty = 0;
-  
-  //clear buffer
-  for (int i=0; i < numLEDs; i++) {
-    setPixelColor(i,0);
-  }
+    prettyUglyCopyOfNumPixels = n;  
+    numLEDs = n;  
+    pixelData = (uint16_t *)malloc(n);
+    isDirty = 0;
+
+    // Clear buffer
+    for (int i = 0; i < numLEDs; i++) {
+        setPixelColor(i, 0);
+    }
 }
-/*the SPI data register (SPDR) holds the byte which is about to be shifted out the MOSI line */
-#define SPI_LOAD_BYTE(data) SPDR=data
-/* Wait until last bytes is transmitted. */
-#define SPI_WAIT_TILL_TRANSMITED while(!(SPSR & (1<<SPIF)))
 
 // Activate hard/soft SPI as appropriate:
 void LPD6803_SPI::begin(uint8_t divider) {
-  startSPI(divider);
+    startSPI(divider);
 }
 
 // Enable SPI hardware and set up protocol details:
 void LPD6803_SPI::startSPI(uint8_t divider) {
-  SPI.begin();
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(divider);  // 0.25 MHz  
-//  SPI.setClockDivider(SPI_CLOCK_DIV2);  // 8 MHz
-//  SPI.setClockDivider(SPI_CLOCK_DIV8);  // 2 MHz
-// SPI.setClockDivider(SPI_CLOCK_DIV16);  // 1 MHz  
-// SPI.setClockDivider(SPI_CLOCK_DIV32);  // 0.5 MHz  
-//  SPI.setClockDivider(SPI_CLOCK_DIV64);  // 0.25 MHz  
-  // LPD6803 can handle a data/PWM clock of up to 25 MHz, and 50 Ohm
-  // resistors on SPI lines for impedance matching.  In practice and
-  // at short distances, 2 MHz seemed to work reliably enough without
-  // resistors, and 4 MHz was possible with a 220 Ohm resistor on the
-  // SPI clock line only.  Your mileage may vary.  Experiment!
-  
-  //SPI_A(0); //maybe, move at the end of the startSPI() method
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setClockDivider(divider); 
+    
+    /* LPD6803 can handle a data/PWM clock of up to 25 MHz, and 50 Ohm
+    resistors on SPI lines for impedance matching.  In practice and
+    at short distances, 2 MHz seemed to work reliably enough without
+    resistors, and 4 MHz was possible with a 220 Ohm resistor on the
+    SPI clock line only.  Your mileage may vary.  Experiment! 
+    
+    Some available modes:
+    SPI.setClockDivider(SPI_CLOCK_DIV2);   // 8 MHz
+    SPI.setClockDivider(SPI_CLOCK_DIV8);   // 2 MHz
+    SPI.setClockDivider(SPI_CLOCK_DIV16);  // 1 MHz  
+    SPI.setClockDivider(SPI_CLOCK_DIV32);  // 0.5 MHz  
+    SPI.setClockDivider(SPI_CLOCK_DIV64);  // 0.25 MHz */
+    
+    // SPI_A(0); //maybe, move at the end of the startSPI() method
 }
 
 uint16_t LPD6803_SPI::numPixels(void) {
-  return numLEDs;
+    return numLEDs;
 }
-
 
 void LPD6803_SPI::show(void) {
-  SPI.transfer(0);	
-  SPI.transfer(0);
-  indx = 0;
-  pixelDataCurrent = pixelData; //reset index
-  nState = 0;
-  isDirty = 0;
-  
-  while(!nState) {
-    //First shift in 32bit “0” as start frame, then shift in all data frame, start 
-  	//frame and data frame both are shift by high-bit, every data is input on DCLK rising edge.
-    register uint16_t command;
-    command = 0x8000 | *(pixelDataCurrent++);       //get current pixel
-  	//SPI_LOAD_BYTE( (command>>8) & 0xFF);
-    //SPI_WAIT_TILL_TRANSMITED;                      	//send 8bits
-    //SPI_LOAD_BYTE( command & 0xFF);
-    //SPI_WAIT_TILL_TRANSMITED;                      	//send 8bits again
-    SPI.transfer( (command>>8) & 0xFF);
-    SPI.transfer( command      & 0xFF);
+    SPI.transfer(0);	
+    SPI.transfer(0);
+    index = 0;
+    pixelDataCurrent = pixelData; //reset index
+    nState = 0;
 
-    if(indx++ >= prettyUglyCopyOfNumPixels) { 
-      nState = 1;
+    while(!nState) {
+        /* First shift in 32bit “0” as start frame, then shift in all data frame, start 
+        frame and data frame both are shift by high-bit, every data is input on DCLK rising edge. */
+        register uint16_t command;
+        command = 0x8000 | *(pixelDataCurrent++); // Get current pixel
+        SPI.transfer((command >> 8) & 0xFF);
+        SPI.transfer( command       & 0xFF);
+
+        if(index++ >= prettyUglyCopyOfNumPixels) { 
+            nState = 1;
+        }
     }
-  }
 }
-
 
 void LPD6803_SPI::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
-  if (n > prettyUglyCopyOfNumPixels) return;
-  
+    if (n > prettyUglyCopyOfNumPixels)
+        return;
+
     /* As a modest alternative to full double-buffering, the setPixel()
-	   function blocks until the serial output interrupt has moved past
-	   the pixel being modified here.  If the animation-rendering loop
-	   functions in reverse (high to low pixel index), then the two can
-	   operate together relatively efficiently with only minimal blocking
-	   and no second pixel buffer required. */
-  while(nState==0); 
+    function blocks until the serial output interrupt has moved past
+    the pixel being modified here.  If the animation-rendering loop
+    functions in reverse (high to low pixel index), then the two can
+    operate together relatively efficiently with only minimal blocking
+    and no second pixel buffer required. */
+    while(nState == 0); 
 
-  uint16_t data = g & 0x1F;
-  data <<= 5;
-  data |= b & 0x1F;
-  data <<= 5;
-  data |= r & 0x1F;
-  data |= 0x8000; 
+    uint16_t data = g & 0x1F;
+    data <<= 5;
+    data |= b & 0x1F;
+    data <<= 5;
+    data |= r & 0x1F;
+    data |= 0x8000; 
 
-  pixelData[n] = data;
+    pixelData[n] = data;
 }
 
-//---
 void LPD6803_SPI::setPixelColor(uint16_t n, uint16_t c) {
-  if (n > prettyUglyCopyOfNumPixels) return;
-
+    if (n > prettyUglyCopyOfNumPixels)
+        return;
+    
     /* As a modest alternative to full double-buffering, the setPixel()
-     function blocks until the serial output interrupt has moved past
-	   the pixel being modified here.  If the animation-rendering loop
-	   functions in reverse (high to low pixel index), then the two can
-	   operate together relatively efficiently with only minimal blocking
-	   and no second pixel buffer required. */
-  while(nState==0); 
+    function blocks until the serial output interrupt has moved past
+    the pixel being modified here.  If the animation-rendering loop
+    functions in reverse (high to low pixel index), then the two can
+    operate together relatively efficiently with only minimal blocking
+    and no second pixel buffer required. */
+    while(nState == 0); 
 
-  pixelData[n] = 0x8000 | c; //the first bit of the color word must be set
+    pixelData[n] = 0x8000 | c; // The first bit of the color word must be set
 }
 
 
--- LPD6803_SPI.h
+++ LPD6803_SPI.h
@@ -1,33 +1,30 @@
 #if (ARDUINO >= 100)
- #include <Arduino.h>
+    #include <Arduino.h>
 #else
- #include <WProgram.h>
- #include <pins_arduino.h>
+    #include <WProgram.h>
+    #include <pins_arduino.h>
 #endif
 
 class LPD6803_SPI {
+    public:
+        LPD6803_SPI(uint16_t n); // Use SPI hardware; specific pins only:
 
- public:
-  // Use SPI hardware; specific pins only:
-  LPD6803_SPI(uint16_t n);
+        void
+            begin(uint8_t divider),
+            show(void),
+            setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b),
+            setPixelColor(uint16_t n, uint16_t c);
+        uint16_t
+            numPixels(void);
 
-  void
-    begin(uint8_t divider),
-    show(void),
-    setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b),
-    setPixelColor(uint16_t n, uint16_t c);
-  uint16_t
-    numPixels(void);
-
- private:
-
-  uint16_t
-    numLEDs;  
-  uint8_t
-    clkpin    , datapin,     // Clock & data pin numbers
-    clkpinmask, datapinmask; // Clock & data PORT bitmasks
-  volatile uint8_t
-    *clkport  , *dataport;   // Clock & data PORT registers
-  void    
-    startSPI(uint8_t divider);
+    private:
+        uint16_t
+            numLEDs;  
+        uint8_t
+            clkpin    , datapin,     // Clock & data pin numbers
+            clkpinmask, datapinmask; // Clock & data PORT bitmasks
+        volatile uint8_t
+            *clkport  , *dataport;   // Clock & data PORT registers
+        void    
+            startSPI(uint8_t divider);
 };
